aa406f634c1226158ce1511cdcc06d0527160715
SwarajTheGOAT <bhagatswaraj8894@gmail.com>
2024-12-31 14:20:07 -0800

    unsaved changes

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -4,5 +4,7 @@
 
 // Your motors, sensors, etc. should go here.  Below are examples
 
-inline pros::Motor intake(1);
+inline pros::Motor intake(3);
+inline pros::Motor lady_brownA(1);
+inline pros::Motor lady_brownA(9);
 inline pros::adi::DigitalOut Clamp('A');
